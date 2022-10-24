32d9eff8db03d3a984fa1c8995f904b80dfc90a6
Jose Gonzalez <jogo00002@stud.uni-saarland.de>
2022-10-24 10:36:11 +0200

    add base route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
+// import the routes
+const baseRoutes = require('./routes/base_routes');
+
 // now create the app constant
 const app = express();
+
+// set the base routes
+app.use(baseRoutes);
 
 //listen on port 3000
 app.listen(3000);
